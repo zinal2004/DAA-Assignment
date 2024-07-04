91a76e251ab25c74a9bb49f6f289c15c1a8ae813
Zinal Shah <153635168+zinal2004@users.noreply.github.com>
2024-07-04 19:02:03 +0530

    Create Question-5.py

---
+++ Python Fundamental Assignment/Question-5.py
@@ -0,0 +1,17 @@
+def is_prime(number):
+    if number <= 1:
+        return False
+    if number == 2:
+        return True
+    if number % 2 == 0:
+        return False
+    for i in range(3, int(number ** 0.5) + 1, 2):
+        if number % i == 0:
+            return False
+    return True
+
+number = int(input("Enter a number: "))
+if is_prime(number):
+    print(f"{number} is a prime number.")
+else:
+    print(f"{number} is not a prime number.")
