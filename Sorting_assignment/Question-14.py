4c3e1bc48aab195fac136bee4042000609f7b514
Zinal Shah <153635168+zinal2004@users.noreply.github.com>
2024-07-13 11:28:59 +0530

    Create Question-14.py

---
+++ Sorting_assignment/Question-14.py
@@ -0,0 +1,10 @@
+class Solution:
+    def frequencySort(self, nums: List[int]) -> List[int]:
+        count = {}
+        for num in nums:
+            if num not in count:
+                count[num] = 1
+            else:
+                count[num] += 1
+        nums.sort(key=lambda x: (count[x], -x))
+        return nums
